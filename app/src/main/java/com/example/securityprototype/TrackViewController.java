6d8a61024cc9490fef65e5da98c68f4993c7e90c
danielvs1234 <danielvs@live.dk>
2019-12-15 04:17:30 +0100

    Update TrackViewController.java

--- app/src/main/java/com/example/securityprototype/TrackViewController.java
+++ app/src/main/java/com/example/securityprototype/TrackViewController.java
@@ -42,62 +42,42 @@ public class TrackViewController {
 
         mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
 
-            // Use default InfoWindow frame
             @Override
             public View getInfoWindow(Marker arg0) {
                 return null;
             }
 
-            // Defines the contents of the InfoWindow
             @Override
             public View getInfoContents(Marker marker) {
 
                 Track track = (Track) marker.getTag();
-                // Getting view from the layout file info_window_layout
+
                 LayoutInflater inflater = LayoutInflater.from(context);
                 View v = inflater.inflate(R.layout.windowlayout, null);
 
-                // Getting the position from the marker
                 LatLng latLng = track.getLatLng();
 
-                // Getting reference to the TextView to set latitude
                 TextView tvLat = v.findViewById(R.id.tv_lat);
-
-                // Getting reference to the TextView to set longitude
                 TextView tvLng = v.findViewById(R.id.tv_lng);
-
                 TextView tvDateTime = v.findViewById(R.id.tv_timeStamp);
 
-                // Setting the latitude
                 tvLat.setText("Latitude: " + track.getLatLng().latitude);
-
-                // Setting the longitude
                 tvLng.setText("Longitude: " + track.getLatLng().longitude);
-
                 tvDateTime.setText("Date: " + track.getDate() + "\n" + "Time: " + track.getTime());
 
-                // Returning the view containing InfoWindow contents
                 return v;
 
             }
         });
 
-        // Adding and showing marker while touching the GoogleMap
         mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
 
             @Override
             public boolean onMarkerClick(Marker marker) {
-                // Creating an instance of MarkerOptions to set position
                 MarkerOptions markerOptions = new MarkerOptions();
-
-                // Setting position on the MarkerOptions
                 markerOptions.position(marker.getPosition());
 
-                // Animating to the currently touched position
                 mMap.animateCamera(CameraUpdateFactory.newLatLng(marker.getPosition()));
-
-
-                // Showing InfoWindow on the GoogleMap
                 marker.showInfoWindow();
 
                 return true;
